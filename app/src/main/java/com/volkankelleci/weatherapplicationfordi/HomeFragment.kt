7826f4da04e8130140ab8f90054feccf92787cfc
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-03-29 12:02:20 +0300

    Menu defined to Main Activity

--- app/src/main/java/com/volkankelleci/weatherapplicationfordi/HomeFragment.kt
+++ app/src/main/java/com/volkankelleci/weatherapplicationfordi/HomeFragment.kt
@@ -2,25 +2,17 @@ package com.volkankelleci.weatherapplicationfordi
 
 import android.os.Bundle
 import android.view.*
-import androidx.core.view.MenuHost
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Lifecycle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.volkankelleci.weatherapplicationfordi.databinding.FragmentHomeBinding
-import kotlinx.coroutines.NonDisposableHandle.parent
 
 
 class HomeFragment : Fragment() {
     private  var _binding:FragmentHomeBinding?=null
     private  val binding get()=_binding!!
 
-    lateinit var bottomNav : BottomNavigationView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
-
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -30,53 +22,9 @@ class HomeFragment : Fragment() {
         val view=binding.root
         return view
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setHasOptionsMenu(true)
-
-        //BottomNav. Define
-        bottomNav.setOnItemSelectedListener {
-            when (it.itemId) {
-                R.id.home_menu -> {
-                    loadFragment(HomeFragment())
-                    true
-                }
-                R.id.search_menu -> {
-                    loadFragment(SearchFragment())
-                    true
-                }
-                R.id.notify_menu -> {
-                    loadFragment(NotifyFragment())
-                    true
-                }
-                R.id.settings_menu -> {
-                    loadFragment(SettingsFragment())
-                    true
-                }
-                else -> {
-                    false
-                }
-            }
-        }
     }
-
-     fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
-        menuInflater.inflate(R.menu.bottom_nav_menu, menu)
-        // Do stuff...
-    }
-
-    fun onMenuItemSelected(menuItem: MenuItem): Boolean {
-        // Do stuff...
-        return false
-    }
-
-    private  fun loadFragment(fragment: Fragment){
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(com.google.android.material.R.id.container,fragment)
-        transaction.commit()
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         _binding
--- app/src/main/java/com/volkankelleci/weatherapplicationfordi/MainActivity.kt
+++ app/src/main/java/com/volkankelleci/weatherapplicationfordi/MainActivity.kt
@@ -1,10 +1,6 @@
 package com.volkankelleci.weatherapplicationfordi
 
 import android.os.Bundle
-import android.provider.Settings
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -12,15 +8,49 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bottomNav: BottomNavigationView
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
 
 
+        //Choiced start fragment which fragment will start
+        jumpFragment(HomeFragment())
+
+        //Defined bottom navigation view
+        bottomNav= findViewById(R.id.bottomNavigationView)
+        bottomNav.setOnItemSelectedListener {MenuItem->
+            when(MenuItem.itemId){
+                R.id.home_menu ->{
+                    jumpFragment(HomeFragment())
+                    true
+                }
+                R.id.search_menu ->{
+                    jumpFragment(SearchFragment())
+                    true
+                }
+                R.id.notify_menu ->{
+                    jumpFragment(NotifyFragment())
+                    true
+                }
+                R.id.settings_menu ->{
+                    jumpFragment(SettingsFragment())
+                    true
+                }
+
+                else -> {false}
+            }
+
+        }
     }
 
-
-
+    // Load fragment to Container
+    private fun jumpFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, fragment)
+            .commit()
+    }
 }
