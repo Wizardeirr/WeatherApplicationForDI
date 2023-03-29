a0bfb3feffa521b3c8c0b5ff1cc0fc386b4e1ada
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-03-29 07:54:33 +0300

    First

--- app/src/main/java/com/volkankelleci/weatherapplicationfordi/MainActivity.kt
+++ app/src/main/java/com/volkankelleci/weatherapplicationfordi/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.volkankelleci.weatherapplicationfordi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_PAN);
+
+
     }
+
+
+
 }
