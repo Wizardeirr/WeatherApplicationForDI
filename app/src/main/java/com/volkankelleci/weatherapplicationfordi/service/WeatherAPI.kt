fc2e32192fc3d4d3b523d7de58ef10c494cd0703
Wizardeirr <113922745+Wizardeirr@users.noreply.github.com>
2023-04-03 22:03:21 +0300

    API Changed

--- app/src/main/java/com/volkankelleci/weatherapplicationfordi/service/WeatherAPI.kt
+++ app/src/main/java/com/volkankelleci/weatherapplicationfordi/service/WeatherAPI.kt
@@ -1,6 +1,7 @@
 package com.volkankelleci.weatherapplicationfordi.service
 
 import com.volkankelleci.model.WeatherData
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -9,9 +10,6 @@ interface WeatherAPI {
     //URL: data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={d97957d008b633c512729f9554ae1c35}
     //API KEY : d97957d008b633c512729f9554ae1c35
     //BASE_URL : https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-    @GET("geo/1.0/direct?q=London&limit=5&appid=")
-    suspend fun weatherSearch(
-        @Query("q") cityName:String,
-        @Query("appid") apiKey:String
-    ): retrofit2.Response<List<WeatherData>>
+    @GET("current?access_key=222ceeb7d76043091460580c254eb5c2&")
+    fun getData(@Query("query") cityName:String):Response<List<WeatherData>>
 }
